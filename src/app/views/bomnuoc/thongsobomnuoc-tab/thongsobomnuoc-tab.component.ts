a66fe2b43ca42534df9c5e309198a5e07f093b3e
Le Quang Hung <hunglq7@gmail.com>
2025-03-11 21:44:11 +0700

    11-03-2025-21h43-thongsobomnuoc-tab

--- src/app/views/bomnuoc/thongsobomnuoc-tab/thongsobomnuoc-tab.component.ts
+++ src/app/views/bomnuoc/thongsobomnuoc-tab/thongsobomnuoc-tab.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { DataService } from '../../../core/services/data.service';
+export interface ThongsoBomNuoc {
+  id: number;
+  tenThietbi: string;
+  noiDung: string;
+  donViTinh: string;
+  thongSo: string;
+}
+
 
 @Component({
   selector: 'app-thongsobomnuoc-tab',
@@ -6,6 +15,29 @@ import { Component } from '@angular/core';
   templateUrl: './thongsobomnuoc-tab.component.html',
   styleUrl: './thongsobomnuoc-tab.component.scss'
 })
-export class ThongsobomnuocTabComponent {
+export class ThongsobomnuocTabComponent implements OnChanges {
+
+  @Input() TonghopbomnuocDetail: any;
+
+  bomNuocId!: number;
+  entity: any;
+  items: any;
+
+  constructor(private dataService: DataService) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this.entity = changes['TonghopbomnuocDetail'].currentValue;
+    this.bomNuocId = this.entity.bomNuocId;
+    this.getDataDetail();
+  }
+
+  getDataDetail() {
+    this.dataService
+      .getById('/api/Thongsobomnuoc/DetailById/' + this.bomNuocId)
+      .subscribe({
+        next: (response) => {
+          this.items = response;
+        },
+      });
+  }
 
 }
