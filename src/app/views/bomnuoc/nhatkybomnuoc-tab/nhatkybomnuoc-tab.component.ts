189b7649ffbc678ab4747c4e23378b24adff60ec
Le Quang Hung <hunglq7@gmail.com>
2025-03-11 21:38:39 +0700

    11-03-2025-21h38-nhatkybomnuoc-tab

--- src/app/views/bomnuoc/nhatkybomnuoc-tab/nhatkybomnuoc-tab.component.ts
+++ src/app/views/bomnuoc/nhatkybomnuoc-tab/nhatkybomnuoc-tab.component.ts
@@ -1,11 +1,165 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { ButtonDirective } from '@coreui/angular';
+import { ToastrService } from 'ngx-toastr';
 
+import {
+  ColDef,
+  GridApi,
+  GridReadyEvent,
+  RowSelectionModule,
+  RowSelectionOptions,
+} from 'ag-grid-community';
+import { DataService } from '../../../core/services/data.service';
+import { AgGridAngular } from 'ag-grid-angular';
+import { RowComponent, ColComponent } from '@coreui/angular';
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule, RowSelectionModule]);
+export interface NhatKyBomNuocDetail {
+  id: number;
+  tonghopbomnuocId: number;
+  ngayLap: string;
+  donVi: string;
+  viTri: string;
+  trangThai: string;
+  ghiChu: string;
+}
 @Component({
   selector: 'app-nhatkybomnuoc-tab',
-  imports: [],
+  imports: [
+    RowComponent, 
+    ColComponent, 
+    ButtonDirective, 
+    AgGridAngular
+  ],
   templateUrl: './nhatkybomnuoc-tab.component.html',
   styleUrl: './nhatkybomnuoc-tab.component.scss'
 })
-export class NhatkybomnuocTabComponent {
+export class NhatkybomnuocTabComponent implements OnChanges {
 
+@Input() TonghopquatgioDetail: any;
+  private gridApi!: GridApi<NhatKyBomNuocDetail>;
+  rowData: NhatKyBomNuocDetail[] = [];
+  tonghopbomnuocId!: number;
+  entity: any;
+
+  constructor(
+    private dataService: DataService,
+    private toastr: ToastrService
+  ) {}
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.entity = changes['TonghopbomnuocDetail'].currentValue;
+    this.tonghopbomnuocId = this.entity.id;
+    this.getDataDetailById();
+  }
+
+  rowSelection: RowSelectionOptions | 'single' | 'multiple' = {
+      mode: 'multiRow',
+    };
+    defaulColDef = {
+      flex: 1,
+      minWith: 100,
+      minHight: 50,
+    };
+    onGridReady(params: GridReadyEvent<NhatKyBomNuocDetail>) {
+      this.gridApi = params.api;
+    }
+
+    onAddRow() {
+      this.gridApi.applyTransaction({
+        add: [
+          {
+            id: 0,
+            tonghopbomnuocId: this.tonghopbomnuocId,
+            ngayLap: '',
+            donVi: '',
+            viTri: '',
+            trangThai: '',
+            ghiChu: '',
+          },
+        ],
+        addIndex: 0,
+      });
+    }
+
+    colDefs: ColDef[] = [
+      {
+        field: 'id',
+        headerName: 'Id',
+        hide: true,
+      },
+      {
+        field: 'tonghopbomnuocId',
+        hide: true,
+      },
+      {
+        field: 'ngaythang',
+        headerName: 'Ngày sử dụng',
+        editable: true,
+        cellEditorPopup: true,
+      },
+      {
+        field: 'donVi',
+        headerName: 'Đơn vị',
+        editable: true,
+        cellEditorPopup: true,
+      },
+  
+      {
+        field: 'viTri',
+        headerName: 'Vị trí',
+        editable: true,
+        cellEditorPopup: true,
+      },
+      {
+        field: 'trangThai',
+        headerName: 'Tình trạng',
+        editable: true,
+        cellEditor: 'agLargeTextCellEditor',
+        cellEditorParams: { maxLength: 10000 },
+      },
+      { field: 'ghiChu', headerName: 'Ghi chú' },
+    ];
+    getDataDetailById() {
+      this.dataService
+        .getById('/api/Nhatkybomnuoc/DatailById/' + this.tonghopbomnuocId)
+        .subscribe({
+          next: (response) => {
+            this.rowData = response;
+          },
+        });
+    }
+    onDelete() {
+      const selectedRows = this.gridApi.getSelectedRows();
+      this.dataService
+        .post('/api/Nhatkybomnuoc/DeleteMultipale', selectedRows)
+        .subscribe({
+          next: () => {
+            this.getDataDetailById();
+            this.toastr.success('Xóa dữ liệu thành công', 'Success');
+          },
+          error: () => {
+            this.toastr.error('Xóa dữ liệu thất bại', 'Error');
+          },
+        });
+    }
+    save() {
+      const selectedRows = this.gridApi.getSelectedRows();
+      this.dataService
+        .put('/api/Nhatkybomnuoc/UpdateMultiple', selectedRows)
+        .subscribe({
+          next: (data: any) => {
+            this.getDataDetailById();
+            this.toastr.success(
+              'Thêm thành công ' + data + ' bản ghi',
+              'Success'
+            );
+          },
+          error: () => {
+            this.toastr.warning('Phải chọn danh sách cần lưu ', 'Warning');
+          },
+        });
+    }
 }
