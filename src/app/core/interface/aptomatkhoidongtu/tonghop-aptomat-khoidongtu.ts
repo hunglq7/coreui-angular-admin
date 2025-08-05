4a1460e816be98917e37fb8f7eb5781bdf479934
hungtmd <hunglq7@gmail.com>
2025-08-05 15:40:30 +0700

    05.08.2025

---
+++ src/app/core/interface/aptomatkhoidongtu/tonghop-aptomat-khoidongtu.ts
@@ -0,0 +1,21 @@
+export interface TongHopAptomatKhoidongtu {
+  id: number;
+  tenThietBi: string;
+  tenDonVi: string;
+  viTriLapDat: string;
+  ngayKiemDinh: string;
+  ngayLap: string;
+  tinhTrang: string;
+  ghiChu: string;
+}
+
+export interface TongHopAptomatKhoidongtuDetail {
+  id: number;
+  thietBiId: number;
+  donViId: number;
+  viTriLapDat: string;
+  ngayKiemDinh: string;
+  ngayLap: string;
+  tinhTrang: string;
+  ghiChu: string;
+}
